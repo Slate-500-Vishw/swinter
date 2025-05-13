4d4a2972dc07fae65bdf116ac0aa13d9e8f158db
VishwRana-Coder <vishw.rana.k@gmail.com>
2025-05-13 15:21:04 +0530

    navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {JetBrains_Mono} from 'next/font/google'
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jetBrain = JetBrains_Mono({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${jetBrain.className}`}>
+        <ThemeProvider 
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/login components/Header.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { IoIosMenu } from "react-icons/io";
+
+const Header = () => {
+  return (
+    <div className="container flex mx-auto justify-between h-[50px] items-center">
+      <Link href={"/"}>
+        <h1 className="text-3xl font-semibold ml-2">
+          Swinter<span className="accent-color">.</span>
+        </h1>
+      </Link>
+      <div className="md:flex hidden mr-2">
+        <div>
+          <ul className="flex items-center gap-5">
+            <li>Home</li>
+            <li>About</li>
+            <li>Features</li>
+            <li>
+              <Button variant={"secondary"}>Login</Button>
+            </li>
+            <li>
+              <Button variant={"secondary"}>Sign Up</Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex md:hidden mr-2">
+        <Sheet>
+          <SheetTrigger>
+            <IoIosMenu />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+            <div>
+              <ul className="flex mr-5 items-end flex-col gap-5">
+                <li>Home</li>
+                <li>About</li>
+                <li>Features</li>
+                <li>
+                  <Button variant={"secondary"}>Login</Button>
+                </li>
+                <li>
+                  <Button variant={"secondary"}>Sign Up</Button>
+                </li>
+              </ul>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
