10a4d2963c97a7730bafc2010df9e314a661d869
VishwRana-Coder <vishw.rana.k@gmail.com>
2025-05-14 13:21:40 +0530

    Let's restart

--- app/theme/page.tsx
+++ app/theme/page.tsx
@@ -1,29 +1,25 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Button, Modal } from "antd";
+import { Modal } from "antd";
 import { ModeToggle } from "@/components/ModeToggle";
-import { useRouter } from "next/navigation";
 
 const ModalComponent = () => {
-  const router = useRouter(); // ✅ Call all hooks at the top
   const [mounted, setMounted] = useState(false);
   const [modal2Open, setModal2Open] = useState(false);
 
   useEffect(() => {
     setMounted(true);
-    setModal2Open(true); 
+    setModal2Open(true);
   }, []);
 
-  if (!mounted) return null; 
+  if (!mounted) return null;
   return (
     <>
       <Modal
         title="Which theme would you like to use?"
         centered
         open={modal2Open}
-        footer={[
-
-        ]}
+        footer={[]}
       >
         <ModeToggle />
       </Modal>
